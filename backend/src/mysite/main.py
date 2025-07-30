64c9e0fca3fdb61e13d9e160ed2af83f6665f279
Christophe762 <64017626+Christophe762@users.noreply.github.com>
2025-07-30 11:59:38 -0400

    infra: Configure MongoDB

--- backend/src/mysite/main.py
+++ backend/src/mysite/main.py
@@ -1,6 +1,18 @@
+import os
+from uuid import UUID, uuid4
+
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel, Field
+
+MONGODB_CONNECTION_STRING = os.environ["MONGODB_CONNECTION_STRING"]
+
+client = AsyncIOMotorClient(MONGODB_CONNECTION_STRING,
+                            uuidRepresentation="standard")
+
+db = client.todolist
+todos = db.todos
 
 app = FastAPI()
 
@@ -13,31 +25,25 @@ app.add_middleware(
 )
 
 class TodoItem(BaseModel):
-    id: int
+    id: UUID = Field(default_factory=uuid4, alias="id")
     content: str
 
 class TodoItemCreate(BaseModel):
     content: str
 
-todos : list[TodoItem] = []
-id_counter = 1
-
 @app.post("/todos",response_model=TodoItem)
 async def create_todo(item : TodoItemCreate):
-    global id_counter
-    new_todo = TodoItem(id=id_counter, content=item.content)
-    todos.append(new_todo)
-    id_counter += 1
+    new_todo = TodoItem(content=item.content)
+    await todos.insert_one(new_todo.model_dump(by_alias=True))
     return new_todo
 
 @app.get("/todos", response_model=list[TodoItem])
 async def read_todos():
-    return todos
+    return await todos.find().to_list(length=None)
 
 @app.delete("/todos/{todo_id}")
 async def delete_todo(todo_id: int):
-    for index, todo in enumerate(todos):
-        if todo.id == todo_id:
-            todos.pop(index)
-            return {"message" : "Todo deleted successfully"}
-        raise HTTPException(status_code=404,detail="Todo not found")
+    delete_result = await todos.delete_one({"_id":todo_id})
+    if delete_result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return {"message": "Todo deleted sucessfully"}
